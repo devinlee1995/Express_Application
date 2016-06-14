abd007f53c23c765ff0d866029c0521056c094b8
Devin <devinlee1995@yahoo.com>
2016-06-14 12:18:19 -0400

    added files

---
+++ app.js
@@ -0,0 +1,34 @@
+//documentation changes all the time so this can change!!!
+//always check online!
+//setting up express
+var express = require('express'); 
+var exphbs = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + '/CSS'));
+
+//a route and we are sending a response 
+app.get('/', function (req, res) {   
+	//res.send('Welcome to Inspiration!'); 
+	res.render('home');
+});
+
+//another route and we are sending a response 
+//from the browser, we go localhost:3000/about and it 
+//displays 'About Inspiration'
+app.get('/about', function (req, res) {   
+	//res.send('About Inspiration'); 
+	res.render('about');
+}); 
+
+app.get('/cupcakes', function (req, res) {   
+	//res.send('Inspirational quotes inspire'); 
+	res.render('cupcakes');
+});
+
+app.listen(3000, function () {  
+	console.log('Inspiration app listening on port 3000!'); 
+});
