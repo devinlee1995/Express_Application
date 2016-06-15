f993f5e80aed8eb258d26ade2b584973b7c8e745
Devin <devinlee1995@yahoo.com>
2016-06-15 14:17:03 -0400

    form

--- app.js
+++ app.js
@@ -3,9 +3,11 @@
 //setting up express
 var express = require('express'); 
 var exphbs = require('express-handlebars');
+var bodyp = require('body-parser');
 
 var app = express();
 
+app.use(bodyp.urlencoded({ extend: true }));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/CSS'));
@@ -31,17 +33,32 @@ app.get('/cupcakes', function (req, res) {
 
 app.get('/contact', function (req, res) {   
 	//res.send('Inspirational quotes inspire'); 
-	res.render('contact');
+	res.render('contact',{username: ""});
 });
 
-app.get('/login', function (req, res) {   
+/*app.get('/login', function (req, res) {   
 	var username = req.query.username;
 	var password = req.query.password;
 	console.log(req.query.username);
 	console.log(req.query.password);
 	res.render('home', {username: username});
+});*/
+
+app.post('/login', function (req, res) {   
+	console.log("hello! This is from the post request");
+	console.log("username: " +req.body.username);
+	var username = req.body.username;
+	if (username != 'bob') {
+		username = "YOU ARE AN IMPOSTER!";
+	}
+	res.render('contact', {username: username});
+});
+
+app.use(function (req, res, next) {
+	res.status(404);
+	res.send('404');
 });
 
 app.listen(3000, function () {  
-	console.log('Inspiration app listening on port 3000!'); 
+	console.log('Cutie Cakes app listening on port 3000!'); 
 });
